0bf03421c6bed1beae37115f8bcf412afc4b1114
ww <15268131701@163.com>
2019-03-14 19:31:43 +0800

    ts数据类型

--- 01Typescript/tsdemo01/index.ts
+++ 01Typescript/tsdemo01/index.ts
@@ -5,3 +5,11 @@ function fn(){
 }
 
 var str:string="ts文件";
+
+/*
+1、vscode配置自动编译
+    1.第一步   tsc --init 生成tsconfig.json   改 "outDir": "./js",  
+    2、第二步 任务 - 运行任务  监视tsconfig.json
+
+
+*/
--- 01Typescript/tsdemo01/js/index.js
+++ 01Typescript/tsdemo01/js/index.js
@@ -1,3 +1,12 @@
 "use strict";
 console.log("He");
-var num = 1;
+function fn() {
+}
+var str = "ts文件";
+/*
+1、vscode配置自动编译
+    1.第一步   tsc --init 生成tsconfig.json   改 "outDir": "./js",
+    2、第二步 任务 - 运行任务  监视tsconfig.json
+
+
+*/ 
